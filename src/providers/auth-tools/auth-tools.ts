73fc00e46164aa8d424f5a6ef9b1f3a33eac8624
CCChira <chira.cristian621@gmail.com>
2019-07-28 01:24:15 +0300

    added providers
    login, logout functions, login state etc.

---
+++ src/providers/auth-tools/auth-tools.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase/app';
+import { GooglePlus, GooglePlusOriginal } from '@ionic-native/google-plus';
+@Injectable()
+export class AuthService {
+    public state = {};
+    public isCorrect = {};
+    constructor(public afAuth: AngularFireAuth, public googleplus: GooglePlusOriginal) {
+        this.isCorrect = 0;
+    }
+    login(email, pass) {
+        return this.afAuth.auth.signInWithEmailAndPassword(email, pass);
+    }
+    /*loginGoogle(){
+        return this.googleplus.login({
+        })*/
+    loginFacebook() {
+        return this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
+    }
+    loginStatus() {
+        this.afAuth.authState.subscribe(state => {
+            this.state = state;
+        });
+    }
+    logOut() {
+        this.googleplus.logout().then(() => {
+            this.afAuth.auth.signOut();
+        },
+        () => {
+            this.afAuth.auth.signOut();
+        });
+    }
+}
+
--- src/validators/password.validator.ts
+++ src/validators/password.validator.ts
@@ -1,13 +1,33 @@
-import {FormControl, FormBuilder, FormGroup, Validator, Validators} from '@angular/forms';
-///check if introduced passwords work
-this.matching_passwords_group = new FormGroup({
-    password: new FormControl('', Validators.compose([
-        Validators.minLength(5),
-        Validators.required,
-        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
-
-    ])),
-    confirm_password: new FormControl('', Validators.required)
-}, (formGroup: FormGroup) => {
-    return this.PasswordValidator.areNotEqual(formGroup);
-});
+import { FormControl } from '@angular/forms';
+export class PasswordValidator {
+    static isCorrect(control: FormControl): any {
+        function validPassword(password) {
+            const rule = /^\w+$/;
+            if (password) {
+                if (password.length < 6) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+            const rule2 = /[0-9]/;
+            if (!rule2.test(password)) {
+                return false;
+            }
+            const rule3 = /[a-zA-Z]/;
+            if (!rule3.test(password)) {
+                return false;
+            }
+            return true;
+        }
+        if (!validPassword(control.value)) {
+            return{
+                'Passwords must have more than six characters, alongside letters and numbers.': true
+            };
+        }
+        return null;
+    }
+    /*static confirmPass(control: FormControl): any {
+        if(control.value == control.root.value)
+    }*/
+}
